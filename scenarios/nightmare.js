3f4c346c21aa5c3e73321a5e1fd6274ce5ad0d80
Илья Элланский <i.ellansky@rabota.ru>
2021-11-16 17:04:30 +0300

    Работает

--- scenarios/nightmare.js
+++ scenarios/nightmare.js
@@ -8,30 +8,26 @@ const chat = new BroadcastChannel('chat')
  * если да – пишет в чат "привет, полуночник"
  */
 let counter = 0
-let isUser = true
-
-const conditions = Promise.all([
-  (counter > 2),
-  isUser,
-  false
-  // условие 3
-  // условие 4
-])
+let isUser = true // какое-то ещё условие
 
 function handleMessage ({ data }) {
   const { id, timestamp } = data
   // проверяем, что событие имеет отношение к данному сценарию
-  if (id === 'VACANCY_CLICK' && timestamp > 0) { // проверку на ночь я опустил
+  if (id === 'VACANCY_CLICK') {
     // да, вот подтверждение
     console.log('Это включился сценарий nightmare.js')
-
     // тогда увеличиваем счётчик
     counter++
 
     // и делаем наши проверочки
-    conditions
-      .then(chat.postMessage("привет, полуночник")) // если все условия выполнены – постим сообщение в чат
-      .catch(console.table) // или смотрим, что пошло не так
+    if (
+      counter >= 3 && 
+      isUser && 
+      timestamp > 0 // что-то про ночь
+      // ещё условие
+      ) {
+        chat.postMessage("привет, полуночник")
+    }
   }
 }
 
